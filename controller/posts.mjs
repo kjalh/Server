22353f9b1ed1c3f0ade0118607ba03f60ff89c8a
희승ACMY <heseung0895@naver.com>
2026-07-03 11:28:15 +0900

    전체 포스트 가져오기 API 완료

---
+++ controller/posts.mjs
@@ -0,0 +1,16 @@
+import * as postRepository from "../data/posts.mjs"
+
+// 포스트를 작성하는 함수
+export async function createPost(req, res) {
+    const { text } = req.body
+    const post = await postRepository.create(text, req.id)
+    res.status(201).json(post)
+}
+
+// 모든 포스트를 가져오는 함수
+export async function getPosts(req, res){
+    const userid = req.query.userid
+    const data = await (userid ? postRepository.getAllByUserid(userid) :
+    postRepository.getAll())
+    res.status(200).json(data)
+}
--- router/posts.mjs
+++ router/posts.mjs
@@ -1,15 +1,20 @@
 import express from "express"
+import { isAuth } from "../middleware/auth.mjs"
+import * as postController from "../controller/posts.mjs"
 
 const router = express.Router()
 
 // 전체 POST 가져오기
-
+// http://127.0.0.1:8080/post (GET)
+// http://127.0.0.1:8080/post?userid=apple  <- 해당 userid의 글만 가져옴
+router.get("/", isAuth, postController.getPosts)
 
 // 글번호에 대한 POST 가져오기
 
 
 // POST 쓰기
-
+// http://127.0.0.1:8080/post
+router.post("/", isAuth, postController.createPost)
 
 // POST 수정하기
 
